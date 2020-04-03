36881c0bac44e67a33981c03c3c4d9e1c14f41e9
NathanCorbin <ncorbin94@live.com>
2020-04-02 22:31:11 -0700

    Add Logo to Navigation component

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import Logo from './Logo'
+
+const Navigation: React.FC = () => (
+  <div style={{ textAlign: 'center' }}>
+    <Logo />
+  </div>
+)
+
+export default Navigation
