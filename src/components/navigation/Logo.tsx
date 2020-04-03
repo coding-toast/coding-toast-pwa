36ce1769f1e849ef37f634655e99c3030f4caaee
NathanCorbin <ncorbin94@live.com>
2020-04-02 22:30:23 -0700

    Create Logo component

---
+++ src/components/navigation/Logo.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import LogoImage from '../../../public/images/logo.png'
+
+const Logo: React.FC = () => (
+  <a href="/">
+    <img src={LogoImage} alt="Logo" width="33%" />
+  </a>
+)
+
+export default Logo
