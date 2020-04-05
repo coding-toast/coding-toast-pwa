f2f84745911802639342e5cad04b31be6bd26457
Quentin Guenther <qguenther@outlook.com>
2020-04-04 21:10:36 -0700

    move blog exerpt fetching logic to Exerpt list

--- src/components/blog-highllights/BlogHighlightSection.tsx
+++ src/components/blog-highllights/BlogHighlightSection.tsx
@@ -1,22 +1,11 @@
 import * as React from 'react'
 import { ExerptList } from './exerpt-list/ExerptList'
-import { CardBody, CardDesctiption, CardImg, CardMeta, CardTitle } from '../card'
 
 const BlogHighlightSection: React.FC<{}> = () => {
   return (
     <section>
       <h1>Catch Up On The Latest</h1>
-      <ExerptList>
-        <CardBody>
-          <CardImg
-            imgUrl="https://images.unsplash.com/profile-1441298803695-accd94000cac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64&s=5a9dc749c43ce5bd60870b129a40902f"
-            altText="Im a picture"
-          />
-          <CardTitle title="Lorem Ipsum" />
-          <CardDesctiption text="abc" />
-          <CardMeta publishDate="November 11, 2016" />
-        </CardBody>
-      </ExerptList>
+      <ExerptList />
     </section>
   )
 }
--- src/components/blog-highllights/exerpt-list/ExerptList.tsx
+++ src/components/blog-highllights/exerpt-list/ExerptList.tsx
@@ -1,5 +1,16 @@
 import * as React from 'react'
+import { CardBody, CardImg, CardTitle, CardDesctiption, CardMeta } from '../../card'
 
-export const ExerptList: React.FC<{}> = ({ children }) => {
-  return <div>{children}</div>
+export const ExerptList: React.FC = () => {
+  return (
+    <CardBody>
+      <CardImg
+        imgUrl="https://images.unsplash.com/profile-1441298803695-accd94000cac?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&cs=tinysrgb&fit=crop&h=64&w=64&s=5a9dc749c43ce5bd60870b129a40902f"
+        altText="Im a picture"
+      />
+      <CardTitle title="Lorem Ipsum" />
+      <CardDesctiption text="abc" />
+      <CardMeta publishDate="November 11, 2016" />
+    </CardBody>
+  )
 }
