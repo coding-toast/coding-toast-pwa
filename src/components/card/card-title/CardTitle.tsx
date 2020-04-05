122ed42330730f5b0c65ce67ad962da31d0815fd
Quentin Guenther <qguenther@outlook.com>
2020-04-05 01:31:06 -0700

    merge CardImg to CardTitle

--- src/components/card/card-title/CardTitle.tsx
+++ src/components/card/card-title/CardTitle.tsx
@@ -2,12 +2,31 @@ import * as React from 'react'
 
 interface CardTitleProps {
   title: string
+  backgroundUrl?: string
 }
 
 const CardTitle: React.FC<CardTitleProps> = props => {
-  const { title } = props
+  const { title, backgroundUrl } = props
 
-  return <h2>{title}</h2>
+  const backgroundStyle = {
+    position: 'relative' as 'relative',
+    height: '124px',
+    backgroundImage: `url(${backgroundUrl})`,
+    backgroundSize: 'cover'
+  }
+
+  const textStyle = {
+    position: 'absolute' as 'absolute',
+    bottom: '12px',
+    padding: 0,
+    margin: 0
+  }
+
+  return (
+    <div style={backgroundUrl ? backgroundStyle : {}}>
+      <h2 style={backgroundUrl ? textStyle : {}}>{title}</h2>
+    </div>
+  )
 }
 
 export default CardTitle
--- src/components/card/card-title/__tests__/CardTitle.spec.tsx
+++ src/components/card/card-title/__tests__/CardTitle.spec.tsx
@@ -4,8 +4,13 @@ import renderer from 'react-test-renderer'
 import CardTitle from '../CardTitle'
 
 describe('<CardTitle />', () => {
-  it('renders correctly', () => {
+  it('renders correctly without a background', () => {
     const tree = renderer.create(<CardTitle title="Lorem Ipsum" />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders correctly with a background', () => {
+    const tree = renderer.create(<CardTitle title="Lorem Ipsum" backgroundUrl="test url" />).toJSON()
     expect(tree).toMatchSnapshot()
   })
 })
