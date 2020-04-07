a5c11698be7a6e28d46411e77d04786bdb60d0d5
NathanCorbin <ncorbin94@live.com>
2020-04-07 00:10:06 -0700

    Update ExerptList.tsx

--- src/components/blog-highllights/exerpt-list/ExerptList.tsx
+++ src/components/blog-highllights/exerpt-list/ExerptList.tsx
@@ -4,35 +4,36 @@ import { CardBody, CardTitle, CardDesctiption, CardMeta } from '../../card'
 interface ExcerptListProps {
   allMarkdownRemark: {
     allMarkdownRemark: {
-      edges: {
-        node: {
-          frontmatter: {
-            author: string
-            banner: string
-            description: string
-            title: string
-            publishDate: string
-          }
-        }
-      }[]
+      edges: Post[]
     }
   }
 }
-
+interface Post {
+  node: {
+    frontmatter: {
+      author: string
+      banner: string
+      description: string
+      title: string
+      publishDate: string
+    }
+  }
+}
 const ExerptList: React.FC<ExcerptListProps> = props => {
   const { allMarkdownRemark } = props
-
   return (
     <section>
-      {allMarkdownRemark.allMarkdownRemark.edges.map((post: any) => (
-        <CardBody>
-          <CardTitle title={post.node.frontmatter.title} backgroundUrl={post.node.frontmatter.banner} />
-          <CardDesctiption text={post.node.frontmatter.description} />
-          <CardMeta publishDate={post.node.frontmatter.publishDate} />
-        </CardBody>
-      ))}
+      {allMarkdownRemark.allMarkdownRemark.edges.map((post: Post) => {
+        const { title, banner, description, publishDate } = post.node.frontmatter
+        return (
+          <CardBody>
+            <CardTitle title={title} backgroundUrl={banner} />
+            <CardDesctiption text={description} />
+            <CardMeta publishDate={publishDate} />
+          </CardBody>
+        )
+      })}
     </section>
   )
 }
-
 export default ExerptList
