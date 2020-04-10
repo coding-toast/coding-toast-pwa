e5baad581ac8bddd439a0969a9a94d51bb8e5a32
QuentinGuenther <QuentinGuenther@users.noreply.github.com>
2020-04-10 06:21:09 +0000

    deployed via Gatsby Publish Action 🎩 for da756f0fc39223f4256d503b9f5b4a8b6e508c1a

---
+++ component---src-pages-404-tsx-6a936a0703be0dbcf4aa.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n("Wbzz"),o=n("NqE+"),u=n("+ego");t.default=function(){return a.createElement(u.a,null,a.createElement(o.a,null,a.createElement("h1",null,"404: Page not found."),a.createElement("p",null,"You've hit the void. ",a.createElement(l.a,{to:"/"},"Go back."))))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-6a936a0703be0dbcf4aa.js.map
