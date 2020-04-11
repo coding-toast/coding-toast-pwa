a55439c29d9b82eb8a88fc4bbbdc3af303352b22
NathanCorbin <ncorbin94@live.com>
2020-04-11 16:38:21 -0700

    Center header logo (#36)
    * Center header logo
    
    * Update Logo.module.scss

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import LogoImage from '../../images/logo.png'
+import LogoStyles from './Logo.module.scss'
 
 const Logo: React.FC = () => (
   <a href="/">
-    <div style={{ textAlign: 'center', maxWidth: '100%', paddingLeft: '30px' }}>
-      <img src={LogoImage} alt="Logo" style={{ maxWidth: '100%' }} />
+    <div className={LogoStyles.logoContainer}>
+      <img className={LogoStyles.logoImage} src={LogoImage} alt="Logo" />
     </div>
   </a>
 )
