cde9ee27d43b7d5a3a3e43c95ca32fc0fff84896
Quentin Guenther <qguenther@outlook.com>
2020-04-10 20:20:12 -0700

    Update deploy-github.js

--- deploy-github.js
+++ deploy-github.js
@@ -4,7 +4,11 @@ ghpages.publish(
   'public',
   {
     branch: 'gh-pages',
-    repo: 'https://' + process.env.GIT_AUTH_TOKEN + '@github.com/QuentinGuenther/coding-toast-pwa.git'
+    repo: 'https://' + process.env.GIT_AUTH_TOKEN + '@github.com/QuentinGuenther/coding-toast-pwa.git',
+    user: {
+      name: 'Quentin Guenther',
+      email: 'qguenther@outlook.com'
+    }
   },
   error => {
     if (error) {
