24e38ed50704101c9d161ea9d69a29897181087d
Quentin Guenther <qguenther@outlook.com>
2020-04-11 21:43:23 -0700

    add toUrl prop and fix tests

--- src/components/buttons/button-with-icon/ButtonWithIcon.tsx
+++ src/components/buttons/button-with-icon/ButtonWithIcon.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 import ButtonWithIconStyles from './ButtonWithIcon.module.scss'
 
-export const ButtonWithIcon: React.FC = () => {
+interface ButtonWithIconProps {
+  toUrl: string
+}
+
+export const ButtonWithIcon: React.FC<ButtonWithIconProps> = props => {
+  const { toUrl } = props
   return (
-    <Link to="" className={`${ButtonWithIconStyles.button} text-style-label`}>
+    <Link to={toUrl} className={`${ButtonWithIconStyles.button} text-style-label`}>
       Read More
     </Link>
   )
