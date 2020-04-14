6aad378caa8463883bb670e3446511e508971860
NathanCorbin <ncorbin94@gmail.com>
2020-04-13 17:53:53 -0700

    Move hamburger button styles out of component

--- src/components/navigation/menu/HamburgerButton.tsx
+++ src/components/navigation/menu/HamburgerButton.tsx
@@ -1,32 +1,14 @@
 import * as React from 'react'
 import HamburgerIcon from '../../../images/hamburger.png'
 import Menu from './Menu'
-
-const HamburgerIconStyles = {
-  color: '#70F3FF',
-  display: 'block',
-  position: 'absolute' as 'absolute',
-  right: 0,
-  top: 0,
-  padding: '0'
-}
-
-const ButtonStyle = {
-  background: 'none',
-  color: 'inherit',
-  border: 'none',
-  padding: 0,
-  font: 'inherit',
-  cursor: 'pointer',
-  outline: 'inherit'
-}
+import HamburgerStyles from './HamburgerButton.module.scss'
 
 const HamburgerButton: React.FC = () => {
   const [open, isOpen] = React.useState(false)
   return (
     <div>
-      <button type="button" className="hamburgerButton" style={ButtonStyle} onClick={() => isOpen(!open)} onKeyDown={() => isOpen(!open)}>
-        <img src={HamburgerIcon} style={HamburgerIconStyles} alt="Open Menu" />
+      <button type="button" className={HamburgerStyles.hamburgerButton} onClick={() => isOpen(!open)} onKeyDown={() => isOpen(!open)}>
+        <img src={HamburgerIcon} className={HamburgerStyles.hamburgerIcon} alt="Open Menu" />
       </button>
       {open ? <Menu /> : ''}
     </div>
