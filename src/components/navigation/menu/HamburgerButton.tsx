a6078fa01f5bedf3bba257a4f3343edeb02aba63
NathanCorbin <ncorbin94@gmail.com>
2020-04-16 00:32:09 -0700

    Update HaamburgerButton.tsx

--- src/components/navigation/menu/HamburgerButton.tsx
+++ src/components/navigation/menu/HamburgerButton.tsx
@@ -4,10 +4,10 @@ import Menu from './Menu'
 import HamburgerStyles from './HamburgerButton.module.scss'
 
 const HamburgerButton: React.FC = () => {
-  const [open, isOpen] = React.useState(false)
+  const [open, setIsOpen] = React.useState(false)
   return (
     <div>
-      <button type="button" className={HamburgerStyles.hamburgerButton} onClick={() => isOpen(!open)} onKeyDown={() => isOpen(!open)}>
+      <button type="button" className={HamburgerStyles.hamburgerButton} onClick={() => setIsOpen(!open)} onKeyDown={() => setIsOpen(!open)}>
         <img src={HamburgerIcon} className={HamburgerStyles.hamburgerIcon} alt="Open Menu" />
       </button>
       {open ? <Menu /> : ''}
