acd9adc1ac8f6ff8c2c8adca8e34cf1fd45b75e2
NathanCorbin <ncorbin94@gmail.com>
2020-04-22 00:12:40 -0700

    cleanup

--- provider-store.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { Provider } from 'mobx-react'
-import RefStore from './src/stores/RefStore'
-
-export default ({ element }) => <Provider RefStore={RefStore}>{element}</Provider>
