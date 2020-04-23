76294c5901e6198b9040d890e30537d6bc22fe88
Quentin Guenther <qguenther@outlook.com>
2020-04-22 22:26:13 -0700

    Fix link and simplify logo container structure

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -1,13 +1,12 @@
 import * as React from 'react'
 import LogoImage from '../../images/logo.svg'
 import LogoStyles from './Logo.module.scss'
+import { Link } from 'gatsby'
 
 const Logo: React.FC = () => (
-  <a href="/">
-    <div className={LogoStyles.logoContainer}>
-      <img className={LogoStyles.logoImage} src={LogoImage} alt="Logo" />
-    </div>
-  </a>
+  <Link to="/" className={LogoStyles.logoContainer}>
+    <img className={LogoStyles.logoImage} src={LogoImage} alt="Logo" />
+  </Link>
 )
 
 export default Logo
