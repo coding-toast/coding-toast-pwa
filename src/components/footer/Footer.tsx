106139668b1d5a51142583733a774b0a72af2361
Quentin Guenther <qguenther@outlook.com>
2020-04-25 20:45:52 -0700

    add footer component (#62)

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+import Container from '../Container'
+import FooterStyles from './Footer.module.scss'
+
+const Footer: React.FC = () => (
+  <Container className={FooterStyles.container}>
+    <h1>CodingToast Footer</h1>
+  </Container>
+)
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,11 +3,13 @@ import * as React from 'react'
 import Page from '../components/Page'
 import IndexLayout from '../layouts'
 import BlogHighlightSection from '../modules/blog-highlights/BlogHighlightSection'
+import Footer from '../components/footer/Footer'
 
 const IndexPage = () => (
   <IndexLayout>
     <Page>
       <BlogHighlightSection />
+      <Footer />
     </Page>
   </IndexLayout>
 )
