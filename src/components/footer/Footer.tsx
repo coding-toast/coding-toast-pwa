7ab37f98328bda6a087062de0b8bbc61de5d2847
NathanCorbin <ncorbin94@live.com>
2020-04-27 22:03:53 -0700

    Ncorbin/footer copyright (#63)
    * Add copyright to footer
    
    * Fix lint errors
    
    * Add footer copyright tests

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react'
 import Container from '../Container'
 import FooterStyles from './Footer.module.scss'
+import Copyright from './Copyright'
 
 const Footer: React.FC = () => (
   <Container className={FooterStyles.container}>
     <h1>CodingToast Footer</h1>
+    <Copyright />
   </Container>
 )
 
---
+++ src/components/footer/__tests__/Copyright.spec.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Copyright from '../Copyright'
+
+describe('<Copyright />', () => {
+  it('renders correctly', () => {
+    const component = shallow(<Copyright />)
+
+    expect(toJson(component)).toMatchSnapshot()
+  })
+})
