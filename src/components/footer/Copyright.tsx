3b862c58649ef9fe03ca39a3510d3338e5c68ee8
NathanCorbin <ncorbin94@gmail.com>
2020-04-28 01:37:48 +0000

    Fix lint errors

--- src/components/footer/Copyright.tsx
+++ src/components/footer/Copyright.tsx
@@ -2,7 +2,11 @@ import * as React from 'react'
 
 const Copyright: React.FC = () => (
   <>
-    <p>&copy; {new Date().getFullYear()} Coding Toast<br />Quentin Guenther, Nathan Corbin</p>
+    <p>
+      &copy; {new Date().getFullYear()} Coding Toast
+      <br />
+      Quentin Guenther, Nathan Corbin
+    </p>
   </>
 )
 
