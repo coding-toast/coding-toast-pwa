d6337c451c87acf064b89be0ab30fbc8b0693699
NathanCorbin <ncorbin94@gmail.com>
2020-04-28 01:33:51 +0000

    Add copyright to footer

---
+++ src/components/footer/Copyright.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const Copyright: React.FC = () => (
+  <>
+    <p>&copy; {new Date().getFullYear()} Coding Toast<br />Quentin Guenther, Nathan Corbin</p>
+  </>
+)
+
+export default Copyright
