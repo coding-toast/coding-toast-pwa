f63ddf68b8ba35d60b2b40cd0e1d1e96de06f366
NathanCorbin <ncorbin94@gmail.com>
2020-04-30 10:23:26 -0700

    Change footer text color to light grey

--- src/components/footer/Copyright.tsx
+++ src/components/footer/Copyright.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
+import FooterStyles from './Footer.module.scss'
 
 const Copyright: React.FC = () => (
-  <>
-    <p>
+  <div>
+    <p className={FooterStyles.copyright}>
       &copy; {new Date().getFullYear()} Coding Toast
       <br />
       Quentin Guenther, Nathan Corbin
     </p>
-  </>
+  </div>
 )
 
 export default Copyright
