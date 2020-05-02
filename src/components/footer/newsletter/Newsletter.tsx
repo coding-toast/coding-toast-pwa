189d3740638a93584e45648d13ac975c30410a89
NathanCorbin <ncorbin94@gmail.com>
2020-05-02 16:41:41 -0700

    Create form and email input

--- src/components/footer/newsletter/Newsletter.tsx
+++ src/components/footer/newsletter/Newsletter.tsx
@@ -2,12 +2,13 @@ import * as React from 'react'
 import Container from '../../Container'
 import NewsletterStyles from './Newsletter.module.scss'
 import SocialMedia from '../SocialMedia'
+import SignUpInfo from './SignUpInfo'
 import SignUpForm from './SignUpForm'
 
 const NewsLetter: React.FC = () => {
   return (
     <Container className={NewsletterStyles.container}>
-      <h1>Newsletter</h1>
+      <SignUpInfo />
       <SignUpForm />
       <SocialMedia styles={NewsletterStyles} />
     </Container>
--- src/components/footer/newsletter/SignUpForm.tsx
+++ src/components/footer/newsletter/SignUpForm.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react'
 import SignUpButton from '../../buttons/button-signup/SignUpButton'
+import NewsletterStyles from './Newsletter.module.scss'
 
 const SignUpForm: React.FC = () => {
   return (
-    <SignUpButton />
+    <form action="">
+      <input type="email" name="email" className={NewsletterStyles.emailInput} />
+      <SignUpButton />
+    </form>
   )
 }
 
