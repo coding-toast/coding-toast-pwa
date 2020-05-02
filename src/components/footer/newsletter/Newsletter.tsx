f7a875175f9a8c21ddaf95676323ed086da65256
Quentin Guenther <qguenther@outlook.com>
2020-05-02 16:01:21 -0700

    fix import

--- src/components/footer/newsletter/Newsletter.tsx
+++ src/components/footer/newsletter/Newsletter.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import NewsletterStyles from './Newsletter.module.scss'
 import SocialMedia from '../SocialMedia'
-import Container, { BackgroundShade } from '../../SectionContainer'
+import Container, { BackgroundShade } from '../../section-container/SectionContainer'
 
 const NewsLetter: React.FC = () => {
   return (
