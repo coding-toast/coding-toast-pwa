d4da959b9355f7e2bf929db7765b292b326e973a
NathanCorbin <ncorbin94@gmail.com>
2020-05-02 15:55:42 -0700

    Create sign up button

--- src/components/footer/newsletter/Newsletter.tsx
+++ src/components/footer/newsletter/Newsletter.tsx
@@ -2,11 +2,13 @@ import * as React from 'react'
 import Container from '../../Container'
 import NewsletterStyles from './Newsletter.module.scss'
 import SocialMedia from '../SocialMedia'
+import SignUpForm from './SignUpForm'
 
 const NewsLetter: React.FC = () => {
   return (
     <Container className={NewsletterStyles.container}>
       <h1>Newsletter</h1>
+      <SignUpForm />
       <SocialMedia styles={NewsletterStyles} />
     </Container>
   )
---
+++ src/components/footer/newsletter/SignUpForm.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+import SignUpButton from '../../buttons/button-signup/SignUpButton'
+
+const SignUpForm: React.FC = () => {
+  return (
+    <SignUpButton />
+  )
+}
+
+export default SignUpForm
