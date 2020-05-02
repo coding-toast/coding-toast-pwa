ce354ce7bf13cb0585d16ee390f373087a940f16
Quentin Guenther <qguenther@outlook.com>
2020-05-02 15:34:47 -0700

    use a row-column format for blog highlight section

--- src/modules/blog-highlights/excerpt-list/ExcerptList.tsx
+++ src/modules/blog-highlights/excerpt-list/ExcerptList.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
+import { Row, Col } from 'react-bootstrap'
 import { CardBody, CardTitle, CardDescription, CardMeta } from '../../../components/card'
-import ExcerptListStyles from './ExcerptList.module.scss'
 import { ButtonWithIcon } from '../../../components/buttons/button-with-icon/ButtonWithIcon'
 
 interface ExcerptListProps {
@@ -24,20 +24,22 @@ interface Post {
 const ExcerptList: React.FC<ExcerptListProps> = (props) => {
   const { allMarkdownRemark } = props
   return (
-    <section className={ExcerptListStyles.list}>
+    <Row as="section" xs={1} md={2}>
       {allMarkdownRemark.allMarkdownRemark.edges.map((post: Post) => {
         const { title, banner, description, publishDate } = post.node.frontmatter
         const { id } = post.node
         return (
-          <CardBody key={id}>
-            <CardTitle title={title} backgroundUrl={banner} />
-            <CardDescription text={description} />
-            <CardMeta publishDate={publishDate} />
-            <ButtonWithIcon toUrl="/" />
-          </CardBody>
+          <Col>
+            <CardBody key={id}>
+              <CardTitle title={title} backgroundUrl={banner} />
+              <CardDescription text={description} />
+              <CardMeta publishDate={publishDate} />
+              <ButtonWithIcon toUrl="/" />
+            </CardBody>
+          </Col>
         )
       })}
-    </section>
+    </Row>
   )
 }
 export default ExcerptList
