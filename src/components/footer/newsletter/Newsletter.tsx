72dabbf47c03fa7906c902933719dc213eeb08fd
NathanCorbin <ncorbin94@gmail.com>
2020-05-02 15:55:42 -0700

    Create sign up button

--- src/components/footer/newsletter/Newsletter.tsx
+++ src/components/footer/newsletter/Newsletter.tsx
@@ -2,11 +2,13 @@ import * as React from 'react'
 import NewsletterStyles from './Newsletter.module.scss'
 import SocialMedia from '../SocialMedia'
 import Container, { BackgroundShade } from '../../section-container/SectionContainer'
+import SignUpForm from './SignUpForm'
 
 const NewsLetter: React.FC = () => {
   return (
     <Container backgroundShade={BackgroundShade.PRIMARY}>
       <h1>Newsletter</h1>
+      <SignUpForm />
       <SocialMedia styles={NewsletterStyles} />
     </Container>
   )
