83647ef00c32c75722914ece5063aa6b2e6903e6
NathanCorbin <ncorbin94@gmail.com>
2020-05-02 16:41:41 -0700

    Create form and email input

--- src/components/footer/newsletter/Newsletter.tsx
+++ src/components/footer/newsletter/Newsletter.tsx
@@ -2,12 +2,13 @@ import * as React from 'react'
 import NewsletterStyles from './Newsletter.module.scss'
 import SocialMedia from '../SocialMedia'
 import Container, { BackgroundShade } from '../../section-container/SectionContainer'
+import SignUpInfo from './SignUpInfo'
 import SignUpForm from './SignUpForm'
 
 const NewsLetter: React.FC = () => {
   return (
     <Container backgroundShade={BackgroundShade.PRIMARY}>
-      <h1>Newsletter</h1>
+      <SignUpInfo />
       <SignUpForm />
       <SocialMedia styles={NewsletterStyles} />
     </Container>
---
+++ src/components/footer/newsletter/SignUpInfo.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import NewsletterStyles from './Newsletter.module.scss'
+
+const SignUpInfo: React.FC = () => {
+  return (
+    <div>
+      <p className={NewsletterStyles.heading}>
+        Sign up for our Newsletter
+      </p>
+      <p className={NewsletterStyles.listHeading}>
+        Get Notified For
+      </p>
+      <ul className={NewsletterStyles.list}>
+        <li>Exclusive Offers</li>
+        <li>Giveaways</li>
+        <li>Updates always important to you!</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SignUpInfo
