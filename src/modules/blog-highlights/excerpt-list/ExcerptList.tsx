eb3ca9172947a03b59047afe63e2686b59a9e26c
Quentin Guenther <qguenther@outlook.com>
2020-05-04 20:16:10 -0700

    change to bootstrap card

--- src/modules/blog-highlights/excerpt-list/ExcerptList.tsx
+++ src/modules/blog-highlights/excerpt-list/ExcerptList.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Card } from 'react-bootstrap'
 import { CardBody, CardTitle, CardDescription, CardMeta } from '../../../components/card'
 import { ButtonWithIcon } from '../../../components/buttons/button-with-icon/ButtonWithIcon'
+import { Link } from 'gatsby'
 
 interface ExcerptListProps {
   allMarkdownRemark: {
@@ -30,12 +31,17 @@ const ExcerptList: React.FC<ExcerptListProps> = (props) => {
         const { id } = post.node
         return (
           <Col>
-            <CardBody key={id}>
-              <CardTitle title={title} backgroundUrl={banner} />
-              <CardDescription text={description} />
-              <CardMeta publishDate={publishDate} />
-              <ButtonWithIcon toUrl="/" />
-            </CardBody>
+            <Card key={id} style={{ margin: '16px 0', backgroundColor: 'rgba(255, 255, 255, 0.05)' }}>
+              <Card.Img variant="top" src={banner} />
+              <Card.Body>
+                <Card.Title className="text-color-primary">{title}</Card.Title>
+                <Card.Subtitle className="mb-2 text-style-muted text-color-secondary">{publishDate}</Card.Subtitle>
+                <Card.Text className="text-color-primary">{description}</Card.Text>
+                <Card.Link>
+                  <Link to="">Read More</Link>
+                </Card.Link>
+              </Card.Body>
+            </Card>
           </Col>
         )
       })}
