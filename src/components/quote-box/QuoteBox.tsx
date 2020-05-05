5f22c92be4c7e9ed64eafd75ece4f06e04d0fd27
Quentin Guenther <qguenther@outlook.com>
2020-05-04 18:34:58 -0700

    fix paddings and html symantics stuff

--- src/components/quote-box/QuoteBox.tsx
+++ src/components/quote-box/QuoteBox.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react'
 import QuoteBoxStyles from './QuoteBox.module.scss'
+import { Jumbotron, Container, Row, Col } from 'react-bootstrap'
+import QuoteImage from '../../images/quote.png'
 
 const QuoteCard: React.FC = () => (
-  <div className={QuoteBoxStyles.quoteBox}>
-    <p className={`text-color-primary text-style-lead ${QuoteBoxStyles.textQuote}`}>
+  <Jumbotron className={QuoteBoxStyles.quoteBox}>
+    <blockquote className={`text-color-primary text-style-lead ${QuoteBoxStyles.textQuote}`}>
       Because it takes more than going over your git log before standup to become a great engineer.
-    </p>
-  </div>
+    </blockquote>
+  </Jumbotron>
 )
 
 export default QuoteCard
