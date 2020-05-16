c7ff58b8a8036fca878e25004ff13e86c0c2a711
Quentin Guenther <qguenther@outlook.com>
2020-05-15 19:02:39 -0700

    Newsletter Fancy Form Validation (#86)
    * install formik
    
    * install yup
    
    * validate newsletter form with formik
    
    * Update jest.config.js

--- src/components/newsletter/SignUpForm.tsx
+++ src/components/newsletter/SignUpForm.tsx
@@ -1,43 +1,66 @@
 import * as React from 'react'
 import { Form, Button, Card, Row, Col } from 'react-bootstrap'
 import { Link } from 'gatsby'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
 
 const SignUpForm: React.FC = () => {
-  const [validated, setValidated] = React.useState(false)
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    const form = event.currentTarget
-    if (form.checkValidity() === false) {
-      event.preventDefault()
-      event.stopPropagation()
-    }
-
-    setValidated(true)
-  }
+  const schema = Yup.object({
+    email: Yup.string().email('Invalid email address').required('Required'),
+    acceptedTerms: Yup.boolean().required('Required').oneOf([true], 'You must accept the terms and conditions.')
+  })
 
   return (
     <Row xs={1} lg={2} className="justify-content-lg-center my-4">
       <Col>
         <Card body style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}>
           <h2 className="mb-4">Never miss a beat with our Newsletter!</h2>
-          <Form noValidate validated={validated} onSubmit={handleSubmit}>
-            <Form.Group controlId="email">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-            </Form.Group>
-            <Form.Group controlId="Terms and Conditions">
-              <Form.Check type="checkbox">
-                <Form.Check.Input type="checkbox" checked />
-                <Form.Check.Label className="small text-muted">
-                  I have read and agree to the <Link to="/">Terms and Conditions</Link> and <Link to="/">Privacy Policy</Link>
-                </Form.Check.Label>
-              </Form.Check>
-            </Form.Group>
-            <Button type="submit" className="btn-accent-secondary" block>
-              Sign Up
-            </Button>
-          </Form>
+          <Formik
+            initialValues={{ email: '', acceptedTerms: true }}
+            validationSchema={schema}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2))
+                setSubmitting(false)
+              }, 400)
+            }}
+          >
+            {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
+              <Form noValidate onSubmit={handleSubmit}>
+                <Form.Group controlId="email">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control
+                    type="email"
+                    placeholder="Enter email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={touched.email && !isValid}
+                    isValid={touched.email && isValid}
+                  />
+                  <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+                  <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                </Form.Group>
+                <Form.Group controlId="acceptedTerms">
+                  <Form.Check type="checkbox">
+                    <Form.Check.Input
+                      type="checkbox"
+                      checked={values.acceptedTerms}
+                      onChange={handleChange}
+                      isInvalid={touched.acceptedTerms && !isValid}
+                    />
+                    <Form.Check.Label className="small text-muted">
+                      I have read and agree to the <Link to="/">Terms and Conditions</Link> and <Link to="/">Privacy Policy</Link>
+                    </Form.Check.Label>
+                    <Form.Control.Feedback type="invalid">{errors.acceptedTerms}</Form.Control.Feedback>
+                  </Form.Check>
+                </Form.Group>
+                <Button type="submit" className="btn-accent-secondary" block>
+                  Sign Up
+                </Button>
+              </Form>
+            )}
+          </Formik>
         </Card>
       </Col>
     </Row>
