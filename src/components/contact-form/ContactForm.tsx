77810eafe0171b1dd0c77fde0f69ae34b1c4760a
Quentin Guenther <qguenther@outlook.com>
2020-05-25 11:18:40 -0700

    create contact form component

---
+++ src/components/contact-form/ContactForm.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+const ContactForm = () => <h1>Contact Form</h1>
+
+export default ContactForm
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react'
 import IndexLayout from '../layouts'
 import Page from '../components/Page'
+import ContactForm from '../components/contact-form/ContactForm'
 
 const ContactPage = () => (
   <IndexLayout>
-    <Page></Page>
+    <Page>
+      <ContactForm />
+    </Page>
   </IndexLayout>
 )
 
