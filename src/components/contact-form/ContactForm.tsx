966af6a2dfe9af27d2f57c24bf6f456b8f18c316
Quentin Guenther <qguenther@outlook.com>
2020-05-25 22:53:16 -0700

    contact page layout

--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -12,88 +12,82 @@ const ContactForm = () => {
     message: Yup.string().max(10000, 'Please shorten your message to 10,000 characters').required('Required')
   })
   return (
-    <SectionContainer backgroundShade={BackgroundShade.PRIMARY}>
-      <Row xs={1} lg={2} className="justify-content-lg-center my-4">
-        <Col>
-          <Card body style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}>
-            <Formik
-              initialValues={{ name: '', email: '', subject: '', message: '' }}
-              validationSchema={schema}
-              onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2))
-                  setSubmitting(false)
-                }, 400)
-              }}
-            >
-              {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
-                <Form noValidate onSubmit={handleSubmit}>
-                  <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="Enter Name"
-                      value={values.name}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isInvalid={touched.name && !!errors.name}
-                      isValid={touched.name && !errors.name}
-                    />
-                    <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId="email">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control
-                      type="email"
-                      placeholder="Enter email"
-                      value={values.email}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isInvalid={touched.email && !!errors.email}
-                      isValid={touched.email && !errors.email}
-                    />
-                    <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId="subject">
-                    <Form.Label>Subject Line</Form.Label>
-                    <Form.Control
-                      as="select"
-                      value={values.subject}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isInvalid={touched.subject && !!errors.subject}
-                    >
-                      <option value="" label="Select a subject line" disabled />
-                      <option value="red" label="red" />
-                      <option value="blue" label="blue" />
-                      <option value="green" label="green" />
-                    </Form.Control>
-                    <Form.Control.Feedback type="invalid">{errors.subject}</Form.Control.Feedback>
-                  </Form.Group>
-                  <Form.Group controlId="message">
-                    <Form.Label>Message</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      placeholder="Your message..."
-                      rows={3}
-                      value={values.message}
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      isInvalid={touched.message && !!errors.message}
-                      isValid={touched.message && !errors.message}
-                    />
-                    <Form.Control.Feedback type="invalid">{errors.message}</Form.Control.Feedback>
-                  </Form.Group>
-                  <Button type="submit" className="btn-accent-secondary" block>
-                    Sign Up
-                  </Button>
-                </Form>
-              )}
-            </Formik>
-          </Card>
-        </Col>
-      </Row>
-    </SectionContainer>
+    <Card body style={{ backgroundColor: 'rgba(255, 255, 255, 0.05)' }}>
+      <Formik
+        initialValues={{ name: '', email: '', subject: '', message: '' }}
+        validationSchema={schema}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2))
+            setSubmitting(false)
+          }, 400)
+        }}
+      >
+        {({ handleSubmit, handleChange, handleBlur, values, touched, isValid, errors }) => (
+          <Form noValidate onSubmit={handleSubmit}>
+            <Form.Group controlId="name">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Name"
+                value={values.name}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.name && !!errors.name}
+                isValid={touched.name && !errors.name}
+              />
+              <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="email">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.email && !!errors.email}
+                isValid={touched.email && !errors.email}
+              />
+              <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="subject">
+              <Form.Label>Subject Line</Form.Label>
+              <Form.Control
+                as="select"
+                value={values.subject}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.subject && !!errors.subject}
+              >
+                <option value="" label="Select a subject line" disabled />
+                <option value="red" label="red" />
+                <option value="blue" label="blue" />
+                <option value="green" label="green" />
+              </Form.Control>
+              <Form.Control.Feedback type="invalid">{errors.subject}</Form.Control.Feedback>
+            </Form.Group>
+            <Form.Group controlId="message">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Your message..."
+                rows={3}
+                value={values.message}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                isInvalid={touched.message && !!errors.message}
+                isValid={touched.message && !errors.message}
+              />
+              <Form.Control.Feedback type="invalid">{errors.message}</Form.Control.Feedback>
+            </Form.Group>
+            <Button type="submit" className="btn-accent-secondary" block>
+              Sign Up
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Card>
   )
 }
 
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,11 +2,34 @@ import * as React from 'react'
 import IndexLayout from '../layouts'
 import Page from '../components/Page'
 import ContactForm from '../components/contact-form/ContactForm'
+import { Form, Row, Col, Card, Button } from 'react-bootstrap'
+import SectionContainer, { BackgroundShade } from '../components/section-container/SectionContainer'
 
 const ContactPage = () => (
   <IndexLayout>
     <Page>
-      <ContactForm />
+      <SectionContainer backgroundShade={BackgroundShade.PRIMARY}>
+        <Row xs={1} lg={2} className="justify-content-center mb-5">
+          <Col className="text-center">
+            <h5 className="text-accent-primary">Got a question?</h5>
+            <h1>Contact Coding Toast</h1>
+            <p className="text-muted lead">
+              We’re here to help and answer any question you might have. We look forward to hearing from you 🙂
+            </p>
+          </Col>
+        </Row>
+        <Row xs={1} lg={2}>
+          <Col>
+            <ContactForm />
+          </Col>
+          <Col className="my-auto">
+            <h1>How Can We Help?</h1>
+            <p className="text-muted">
+              Please select a topic below related to your inquiry. If you don’t find what you need, fill out our contact form.
+            </p>
+          </Col>
+        </Row>
+      </SectionContainer>
     </Page>
   </IndexLayout>
 )
