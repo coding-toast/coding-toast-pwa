aa026c436012aaf8a0fbcab0cc7b08ad69627140
Quentin Guenther <qguenther@outlook.com>
2020-06-04 20:40:13 -0700

    style nav

--- src/components/navigation-panel/NavigationPanel.tsx
+++ src/components/navigation-panel/NavigationPanel.tsx
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 import Logo from '../navigation/logo/Logo'
 
 const NavigationPanel: React.FC = () => (
-  <Navbar collapseOnSelect expand={false} fixed="top" sticky="top" bg="dark" variant="dark">
+  <Navbar collapseOnSelect expand={false} fixed="top" sticky="top" bg="dark" variant="dark" className="bg-background-primary">
     <Navbar.Brand>
       <Link to="/">
         <Logo />
@@ -12,20 +12,26 @@ const NavigationPanel: React.FC = () => (
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="flex-column">
+      <Nav className="text-right">
         <Nav.Item>
           <Nav.Link>
-            <Link to="/m">Blog</Link>
+            <Link to="/m" className="text-text-color-primary">
+              Blog
+            </Link>
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link>
-            <Link to="/m">Resources</Link>
+            <Link to="/m" className="text-text-color-primary">
+              Resources
+            </Link>
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link>
-            <Link to="/contact">Contact</Link>
+            <Link to="/contact" className="text-text-color-primary">
+              Contact
+            </Link>
           </Nav.Link>
         </Nav.Item>
       </Nav>
