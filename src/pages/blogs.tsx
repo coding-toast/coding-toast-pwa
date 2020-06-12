6e1327ad380a10a9d79f64ed7d09ee6eb62018c8
NathanCorbin <ncorbin94@gmail.com>
2020-06-12 12:39:07 -0700

    use graphql to retrieve all blogs

--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -2,15 +2,37 @@ import * as React from 'react'
 import IndexLayout from '../layouts'
 import Page from '../components/Page'
 import SectionContainer, { BackgroundShade } from '../components/section-container/SectionContainer'
+import { useStaticQuery, graphql } from 'gatsby'
+import ExcerptList from '../modules/blog-highlights/excerpt-list/ExcerptList'
 
-const BlogsPage = () => (
-  <IndexLayout>
-    <Page>
-      <SectionContainer backgroundShade={BackgroundShade.SECONDARY}>
-        <h1>BLOGS PAGE</h1>
-      </SectionContainer>
-    </Page>
-  </IndexLayout>
-)
+const BlogsPage = () => {
+  const query = useStaticQuery(
+    graphql`
+      query {
+        allStrapiBlogs {
+          edges {
+            node {
+              blogId
+              author
+              banner
+              title
+              description
+              content
+            }
+          }
+        }
+      }
+    `
+  )
+  return (
+    <IndexLayout>
+      <Page>
+        <SectionContainer backgroundShade={BackgroundShade.PRIMARY}>
+          <ExcerptList allStrapiBlogs={query} />
+        </SectionContainer>
+      </Page>
+    </IndexLayout>
+  )
+}
 
 export default BlogsPage
