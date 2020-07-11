3c8e8baeae98a6b84cdf6537dc19d36c71676e66
Quentin Guenther <qguenther@outlook.com>
2020-07-10 23:37:51 -0700

    add author and sharing to blog page

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -11,9 +11,15 @@ import ReactMarkdown from 'react-markdown';
 import CodeBlock from 'components/code/CodeBlock';
 import InlineCode from 'components/code/InlineCode';
 import { logEvent, logException } from 'utils/analytics';
+import { EmailShareButton, LinkedinShareButton, TwitterShareButton } from 'react-share';
+import { EmailIcon, LinkedinIcon, TwitterIcon } from 'react-share';
 
 const Post: React.FC = () => {
   const router = useRouter();
+  const [url, setUrl] = React.useState('');
+  React.useEffect(() => {
+    setUrl(window.location.href);
+  });
   const { slug } = router.query;
 
   const { data, loading, error } = useQuery(GET_BLOG, { variables: { slug } });
@@ -41,13 +47,24 @@ const Post: React.FC = () => {
   return (
     <MainLayout>
       <SectionContainer backgroundShade={BackgroundShade.PRIMARY}>
-        <h1>{blog.title}</h1>
-        <h2 className='text-muted h3'>
-          published <DateDisplay date={new Date(blog.publishDate)} />
-        </h2>
+        <header>
+          <h1>{blog.title}</h1>
+          <h2 className='text-muted h3'>
+            published <DateDisplay date={new Date(blog.publishDate)} />
+          </h2>
+          <div className='px-0 py-1 mt-3' style={{ height: '3em' }}>
+            <Image
+              src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}${blog.author.avatar.url}`}
+              style={{ display: 'inline-block', height: '100%', borderRadius: '50%' }}
+            />
+            <p style={{ display: 'inline-block', fontSize: '1.15em' }} className='mx-2 text-muted'>
+              {blog.author.displayName}
+            </p>
+          </div>
+        </header>
         <hr />
         <Image src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}${blog.banner.url}`} alt={blog.banner.alternativeText} fluid />
-        <div className='mx-auto my-5' style={{ width: '80vw', maxWidth: '800px', letterSpacing: '0.05em', wordWrap: 'break-word' }}>
+        <article className='mx-auto my-5' style={{ width: '80vw', maxWidth: '800px', letterSpacing: '0.05em', wordWrap: 'break-word' }}>
           <ReactMarkdown
             source={blog.content}
             linkTarget='_blank'
@@ -56,12 +73,30 @@ const Post: React.FC = () => {
               inlineCode: InlineCode
             }}
           />
+        </article>
+        <div className='px-0 py-1 mt-3' style={{ height: '3em' }}>
+          <Image
+            src={`${process.env.NEXT_PUBLIC_API_ENDPOINT}${blog.author.avatar.url}`}
+            style={{ display: 'inline-block', height: '100%', borderRadius: '50%' }}
+          />
+          <p style={{ display: 'inline-block', fontSize: '1.15em' }} className='mx-2 text-muted'>
+            {blog.author.displayName}
+          </p>
         </div>
-        <Image
-          src={blog.author.avatar.url ? `${process.env.NEXT_PUBLIC_API_ENDPOINT}${blog.author.avatar.url}` : '/user-solid.svg'}
-          alt={blog.author.avatar.alternativeText}
-        />
-        <p>{blog.author.displayName}</p>
+        <hr />
+        <aside className='text-center mx-auto'>
+          <h1>Did you like this article?</h1>
+          <p>It would mean a lot to us if you would share it too!</p>
+          <TwitterShareButton url={url}>
+            <TwitterIcon />
+          </TwitterShareButton>
+          <LinkedinShareButton url={url} title={blog.title} summary={blog.description} source='Coding Toast'>
+            <LinkedinIcon />
+          </LinkedinShareButton>
+          <EmailShareButton url={url} subject={blog.title} body={blog.description}>
+            <EmailIcon />
+          </EmailShareButton>
+        </aside>
       </SectionContainer>
     </MainLayout>
   );
